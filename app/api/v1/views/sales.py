e267dfe410c8d28e8573c6a5475034becdc1800b
Nicanor008 <nicanorkorir008@gmail.com>
2018-10-18 15:55:08 +0300

    [ft-#161314317] Endpoints to fetch sales,post a asale and fetch an individual sale

---
+++ app/api/v1/views/sales.py
@@ -0,0 +1,57 @@
+from flask import Flask, abort, request, make_response, jsonify, Blueprint
+from flask_restful import Resource, Api
+from app.api.v1.models.sales import SalesData
+
+# sales list
+sale = SalesData().get_sales()
+
+
+class Sales(Resource):
+    def get(self):
+        """ 
+            Obtain and post sales
+        """
+        return jsonify({
+            'Sales': sale
+        })
+
+
+    def post(self):
+        """create a sale"""
+        sales_data = request.get_json()
+
+        # users data entered, stored in variables
+        sales_id = sales_data['salesId']
+        category = sales_data['category']
+        sale_name = sales_data['product_name']
+        quantity = sales_data['quantity']
+        price = sales_data['price']
+        
+        # check if product is available in the products list
+
+        # store products in a dictionary
+        sales_cart = {
+            "salesId":sales_id, 
+            "category":category, 
+            "product_name":sale_name, 
+            "quantity":quantity,
+            "price":price
+        }
+        # add sale product to the sale list
+        sale.append(sales_cart)
+
+        # message to be displayed 
+        return jsonify({'response':'New Sale recorded'})
+
+
+class GetSingleSale(Resource):   
+    def get(self, salesId):
+        """
+            Get only a single sale using saleid
+            param : Store Owner/admin and store attendant of the specific sale record
+        """
+        for sales in sale:
+            if sales['salesId'] == salesId:
+                return jsonify({"sale":sales})
+
+        return jsonify({"message":"Product Not Available"})     
