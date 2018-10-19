0db44e939ee398c2fea62036851d9c177583696f
Nicanor008 <nicanorkorir008@gmail.com>
2018-10-19 16:25:03 +0300

    welcome to store

--- run.py
+++ run.py
@@ -4,5 +4,9 @@ from app import create_app
 config_name = os.getenv('APP_SETTINGS')
 app = create_app()
 
+app.route('/')
+def index():
+    return "<h1> Welcome to Nicanor Store Manager</h1>"
+
 if __name__ == '__main__':
     app.run(debug=True)
