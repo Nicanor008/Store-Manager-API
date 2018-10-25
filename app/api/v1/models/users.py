a9589ea0bcfdba6809de1db076deafca4e930b84
Nicanor008 <nicanorkorir008@gmail.com>
2018-10-25 08:55:00 +0300

    [ft-#161405940] Have added validation details in login and registration and used a list data structure and not a dict as before

--- app/api/v1/models/users.py
+++ app/api/v1/models/users.py
@@ -1,39 +1,14 @@
-import string
-from flask import jsonify
-from werkzeug.security import generate_password_hash, check_password_hash
+users = []
 
-# users_data = {}
-
-class Users():
-    '''class to represent users model'''
-    def __init__(self):
-        self.user = {} # single user
-        self.users_data = {}
-
-    def put(self, name, username, email, password,role):
-        '''add a user to users_data'''
-        if username in self.users_data:
-            return {"message":"Username already exists"}
-        
-        self.user["name"] = name
-        self.user["email"] = email
-        self.user["username"] = username
-        self.user["role"] = role
-        pw_hash = generate_password_hash(password)
-        self.user["password"] = pw_hash
-
-        self.users_data[email] = self.user
-        return {"message":"{} registered successfully".format(email)}
-
-    def user_verify(self, email, password):
-        '''verify the password a user enters while logging in'''
-        if email in self.users_data:
-            result = check_password_hash(self.users_data[email]["password"], password)
-            if result is True:
-                return "True"
-            return {"message": "The password you entered is incorrect"}
-        return {"message": "email does not exist in our records"}
-    def get_user(self,email, role):
-        if email in self.users_data and role in self.users_data:
-            return self.users_data[email]
-        return {"message":"User not found"}
+class User():
+    def save_user(self, email, name, password, role):
+        user_id = len(users) + 1
+        user = {
+            "user_id": user_id,
+            "email": email,
+            "name":name,
+            "password": password,
+            "role": role
+        }
+        users.append(user)
+        return user    
--- app/api/v1/views/users.py
+++ app/api/v1/views/users.py
@@ -2,76 +2,75 @@ import re
 from flask import request, jsonify
 from flask_restful import Resource
 from flask_jwt_extended import (JWTManager, jwt_required, create_access_token, get_raw_jwt)
-from ..models import users
+from app.api.v1.models.users import User, users
 
-users_list = users.Users()
+# The required format of an email-address
+email_format = r"(^[a-zA-z0-9_.]+@[a-zA-z0-9-]+\.[a-z]+$)"
 
+# Users data list
+user = User()
 
-class Login(Resource):
-    """login in registered users     
-    Returns:
-        token and confirmation message 
+class Login(Resource, User):
+    """API endpoint to login the registered users
+    Returns jsonify users data
     """
     def post(self):
         data = request.get_json()
+        email = data["email"]
+        password =data["password"]
+
         if not data:
-            return jsonify({"message": "Email and password required"})
-
-        email = data.get('email')
-        password = data.get('password')
-        role = data.get('role')
-
-        # if email is not user
-        # if email is not users_list.user_verify
+            return jsonify("Fields cannot be empty")
         if not email or not password:
-            return jsonify({"message": "Username or password missing"})
+            return jsonify("You must provide username and password")
+            
+        if not re.match(email_format, email):
+            return jsonify({"message": "Invalid Email address"})  
 
-        authorize = users_list.user_verify(email, password)
-        user=users_list.get_user(email, role)
+        user_exists = [user for user in users if email == user["email"]]
 
-        if not authorize:
-            return "failed"
-
-        access_token = create_access_token(identity=user)
+        if not user_exists:
+            return jsonify({
+                "message":"User does not exist"
+            })
+        if password != user_exists[0]["password"]:
+            return jsonify({
+                "message":"Wrong password",
+                "status": 400
+            })
+        
+        access_token = create_access_token(identity=email)
         return jsonify(token = access_token, message = "Login successful!")
-        # else:
-            # return "failed"
         
-        
-        
-class Register(Resource):
-    """
-        register new users 
-        Returns:
-            users data in a list
+   
+
+class Register(Resource, User):
+    """Admin to register users
+        Returns jsonify users data accordingly
     """
     def post(self):
         data = request.get_json()
 
-        # users input
-        name = data.get('name')
-        username = data.get('username')
-        email = data.get('email')
-        password = data.get('password')
-        # confirm_password = data['confirm_password']
-        role=data.get('role')
+        email = data["email"]
+        password =data["password"]
+        role = data["role"]  
+        name = data["name"]     
 
-        roles=["owner","admin","attendant"]
-        
-        if role not in roles:
-            return jsonify({"message":"The role {} does not exist".format(role)})
-       
-        # email format
-        email_format = re.match(r'^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$', email)
+        if not data:
+            return jsonify("Data must be in json format")
+        if not email or not password:
+            return jsonify({"message":"You must provide email and password"})
 
-        # check if email is a valid 
-        if email_format is None:
-            return jsonify({"message": "invalid email address"})
+        if not re.match(email_format, email):
+            return jsonify({"message": "Invalid email address"})        
 
-        # if password is not confirm_password:
-            # return jsonify({'message':'passwords do not match'})
+        user_exists = [user for user in users if email == user["email"]]
 
-        response = jsonify(users_list.put(name, username, email, password,role))
-        response.status_code = 201
-        return response
+        if user_exists:
+            return jsonify({"message":"Email address already exists"})
 
+        else:
+            user.save_user(email,name, password, role)
+            return jsonify({
+                "message":"User has been registered successfully"
+            })
